65f3c52d128d2274d2dd2764e01f1d0a56ca88c2
safin-dr <safin.dr@phystech.edu>
2020-09-27 20:10:03 +0300

    Продвинутая картинка 1_2

--- lab3/pic_1.py
+++ lab3/pic_1.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.draw import *
+import numpy as np
 
 pygame.init()
 
@@ -9,50 +10,79 @@ screen = pygame.display.set_mode((600, 402))
 back_color = (255, 255, 0)
 screen.fill(back_color)
 
-rect(screen, (0, 0, 255), (0, 0, 600, 260))
-circle(screen, (0, 0, 0), (360, 190), 35)
-circle(screen, (139, 80, 20), (360, 190), 35) #Boat_start
-rect(screen, (0, 0, 255), (0, 0, 600, 190))
-rect(screen, (0, 0, 255), (360, 190, 35, 35))
-rect(screen, (129, 218, 247), (0, 0, 600, 170))
-rect(screen, (0, 0, 0), (360, 190, 143, 35))
-rect(screen, (139, 80, 20), (361, 190, 141, 35))
-polygon(screen, (0, 0, 0), [(503, 190), (503, 224),(560,190)])
-polygon(screen, (139, 80, 20), [(503, 190), (503, 224),(580,190)])
-circle(screen, (0, 0, 0), (525, 203), 10)
-circle(screen, (255, 255, 255), (525, 203), 7)
-rect(screen, (0, 0, 0), (420, 110, 6, 80))
-polygon(screen, (0, 0, 0), [(426,110), (440, 150), (480, 150)])
-polygon(screen, (218, 173, 128), [(427,111), (441, 149), (478, 149)])
-polygon(screen, (0, 0, 0), [(426,190), (440, 150), (480, 150)])
-polygon(screen, (218, 173, 128), [(427,189), (441, 151), (478, 151)]) #Boat_end
-circle(screen, (255, 255, 0), (540, 60), 40) #Sun
-circle(screen, (0, 0, 0), (170, 40), 16) #Cloud0
-circle(screen, (255, 255, 255), (170, 40), 15)
-circle(screen, (0, 0, 0), (160, 55), 16) #Cloud1
-circle(screen, (255, 255, 255), (160, 55), 15)
-circle(screen, (0, 0, 0), (190, 40), 16) #Cloud2
-circle(screen, (255, 255, 255), (190, 40), 15)
-circle(screen, (0, 0, 0), (180, 55), 16) #Cloud3
-circle(screen, (255, 255, 255), (180, 55), 15)
-circle(screen, (0, 0, 0), (200, 55), 16) #Cloud4
-circle(screen, (255, 255, 255), (200, 55), 15)
-circle(screen, (0, 0, 0), (210, 40), 16) #Cloud5
-circle(screen, (255, 255, 255), (210, 40), 15)
-circle(screen, (0, 0, 0), (220, 55), 16) #Cloud6
-circle(screen, (255, 255, 255), (220, 55), 15)
-rect(screen, (210, 110, 34), (110, 220, 8, 140)) #Umbrella
-polygon(screen, (249, 96, 75), [(118, 220), (118, 245), (172, 245)])
-polygon(screen, (249, 96, 75), [(110, 220), (110, 245), (56, 245)])
-rect(screen, (249, 96, 75), (110, 220, 8, 25))
-line(screen, (0, 0, 0), (110, 220), (71, 245), 1)
-line(screen, (0, 0, 0), (110, 220), (86, 245), 1)
-line(screen, (0, 0, 0), (110, 220), (101, 245), 1)
-line(screen, (0, 0, 0), (118, 220), (127, 245), 1)
-line(screen, (0, 0, 0), (118, 220), (142, 245), 1)
-line(screen, (0, 0, 0), (118, 220), (157, 245), 1)
-line(screen, (0, 0, 255), (118, 220), (118, 245), 1)
-line(screen, (0, 0, 255), (110, 220), (110, 245), 1)
+def multicloud (main_cloud_x, main_cloud_y, cloud_radius, ro_cloud):
+	circle(screen, (0, 0, 0), (main_cloud_x, main_cloud_y), cloud_radius + 1) #Cloud0
+	circle(screen, (255, 255, 255), (main_cloud_x, main_cloud_y), cloud_radius)
+	circle(screen, (0, 0, 0), (main_cloud_x - ro_cloud * 2, main_cloud_y + ro_cloud * 3), cloud_radius + 1) #Cloud1
+	circle(screen, (255, 255, 255), (main_cloud_x - ro_cloud * 2, main_cloud_y + ro_cloud * 3), cloud_radius)
+	circle(screen, (0, 0, 0), (main_cloud_x + ro_cloud * 3, main_cloud_y), cloud_radius + 1) #Cloud2
+	circle(screen, (255, 255, 255), (main_cloud_x + ro_cloud * 3, main_cloud_y), cloud_radius)
+	circle(screen, (0, 0, 0), (main_cloud_x + ro_cloud * 2, main_cloud_y + ro_cloud * 3), cloud_radius + 1) #Cloud3
+	circle(screen, (255, 255, 255), (main_cloud_x + ro_cloud * 2, main_cloud_y + ro_cloud * 3), cloud_radius)
+	circle(screen, (0, 0, 0), (main_cloud_x + ro_cloud * 6, main_cloud_y + ro_cloud * 3), cloud_radius + 1) #Cloud4
+	circle(screen, (255, 255, 255), (main_cloud_x + ro_cloud * 6, main_cloud_y + ro_cloud * 3), cloud_radius)
+	circle(screen, (0, 0, 0), (main_cloud_x + ro_cloud * 8, main_cloud_y), cloud_radius + 1) #Cloud5
+	circle(screen, (255, 255, 255), (main_cloud_x + ro_cloud * 8, main_cloud_y), cloud_radius)
+	circle(screen, (0, 0, 0), (main_cloud_x + ro_cloud * 10, main_cloud_y + ro_cloud * 3), cloud_radius + 1) #Cloud6
+	circle(screen, (255, 255, 255), (main_cloud_x + ro_cloud * 10, main_cloud_y + ro_cloud * 3), cloud_radius)
+
+def umbrella(left_upper_x, left_upper_y, dx_umb, h_umb, umb_radius, umb_start, deltha):
+	rect(screen, (210, 110, 34), (left_upper_x, left_upper_y, dx_umb, h_umb))
+	polygon(screen, (249, 96, 75), [(left_upper_x + dx_umb, left_upper_y), (left_upper_x + dx_umb, left_upper_y + umb_start), (left_upper_x + dx_umb + umb_radius, left_upper_y + umb_start)])
+	polygon(screen, (249, 96, 75), [(left_upper_x, left_upper_y), (left_upper_x, left_upper_y + umb_start), (left_upper_x - umb_radius, left_upper_y + umb_start)])
+	rect(screen, (249, 96, 75), (left_upper_x, left_upper_y, dx_umb, umb_start))
+	aaline(screen, (0, 0, 0), (left_upper_x, left_upper_y), (left_upper_x - umb_radius + 1 * deltha, left_upper_y + umb_start))
+	aaline(screen, (0, 0, 0), (left_upper_x, left_upper_y), (left_upper_x - umb_radius + 2 * deltha, left_upper_y + umb_start))
+	aaline(screen, (0, 0, 0), (left_upper_x, left_upper_y), (left_upper_x - umb_radius + 3 * deltha, left_upper_y + umb_start))
+	aaline(screen, (0, 0, 0), (left_upper_x + dx_umb, left_upper_y), (left_upper_x + dx_umb + umb_radius - 3 * deltha, left_upper_y + umb_start))
+	aaline(screen, (0, 0, 0), (left_upper_x + dx_umb, left_upper_y), (left_upper_x + dx_umb + umb_radius - 2 * deltha, left_upper_y + umb_start))
+	aaline(screen, (0, 0, 0), (left_upper_x + dx_umb, left_upper_y), (left_upper_x + dx_umb + umb_radius - 1 * deltha, left_upper_y + umb_start))
+	aaline(screen, (0, 0, 0), (left_upper_x + dx_umb, left_upper_y), (left_upper_x + dx_umb, left_upper_y + umb_start))
+	aaline(screen, (0, 0, 0), (left_upper_x, left_upper_y), (left_upper_x, left_upper_y + umb_start))
+	
+def draw_paluba(boat_0_x, boat_0_y, proportion):
+	circle(screen, (139, 80, 20), (boat_0_x, boat_0_y), proportion * 7)
+	rect(screen, (0, 0, 255), (boat_0_x - proportion * 7, boat_0_y - proportion * 7, proportion * 7 * 2, proportion * 7))
+	rect(screen, (0, 0, 255), (boat_0_x, boat_0_y, proportion * 7, proportion * 7))
+	rect(screen, (0, 0, 0), (boat_0_x, boat_0_y, proportion * 28 + 2, proportion * 7))
+	rect(screen, (139, 80, 20), (boat_0_x + 1, boat_0_y, proportion * 28, proportion * 7))
+	polygon(screen, (0, 0, 0), [(boat_0_x + proportion * 28 + 2, boat_0_y), (boat_0_x + proportion * 28 + 2, boat_0_y + proportion * 7 - 1), (boat_0_x + proportion * 43, boat_0_y)])
+	polygon(screen, (139, 80, 20), [(boat_0_x + proportion * 28 + 2, boat_0_y), (boat_0_x + proportion * 28 + 2, boat_0_y + proportion * 7 - 1), (boat_0_x + proportion * 43, boat_0_y)])
+	circle(screen, (0, 0, 0), (boat_0_x + proportion * 33, boat_0_y + proportion * 3 - 2), proportion * 2)
+	circle(screen, (255, 255, 255), (boat_0_x + proportion * 33, boat_0_y + proportion * 3 - 2), proportion * 2 - 3)
+	
+def draw_parus(parus_x, parus_y, proportion):
+	rect(screen, (0, 0, 0), (parus_x, parus_y, proportion * 2 - 2,  2 * 10 * proportion))
+	polygon(screen, (218, 173, 128), [(parus_x + proportion * 2 - 2, parus_y), (parus_x + proportion * 6, parus_y + proportion * 10), (parus_x + proportion * 14, parus_y + proportion * 10)])
+	polygon(screen, (218, 173, 128), [(parus_x + proportion * 2 - 2,parus_y + proportion * 10 * 2), (parus_x + proportion * 6, parus_y + proportion * 10), (parus_x + proportion * 14, parus_y + proportion * 10)])
+	aaline(screen, (0, 0, 0), (parus_x + proportion * 6, parus_y + proportion *10), (parus_x + proportion * 14, parus_y + proportion * 10))
+	aaline(screen, (0, 0, 0), (parus_x + proportion * 6, parus_y + proportion *10), (parus_x + proportion * 2 - 2, parus_y))
+	aaline(screen, (0, 0, 0), (parus_x + proportion * 14, parus_y + proportion *10), (parus_x + proportion * 2 - 2, parus_y))
+	aaline(screen, (0, 0, 0), (parus_x + proportion * 6, parus_y + proportion *10), (parus_x + proportion * 2 - 2, parus_y + 2 * 10 * proportion))
+	aaline(screen, (0, 0, 0), (parus_x + proportion * 14, parus_y + proportion *10), (parus_x + proportion * 2 - 2, parus_y + 2 * 10 * proportion))
+	
+R = 40
+r = 8
+Sunlights = 60
+	
+rect(screen, (0, 0, 255), (0, 0, 600, 260))	
+draw_paluba(360, 190, 5)
+draw_paluba(185, 170, 3)
+rect(screen, (0, 0, 255), (0, 0, 600, 170))
+rect(screen, (129, 218, 247), (0, 0, 600, 160))
+draw_parus(415, 90, 5)
+draw_parus(225, 110, 3)
+circle(screen, (255, 255, 0), (540, 60), R) #Sun
+multicloud(170, 40, 15, 5)
+multicloud(306, 30, 25, 8)
+multicloud(116, 98, 19, 7)
+
+for i in range(60):
+	polygon(screen, (255, 255, 0), [(540 + R * np.sin ( 2 * i * np.pi / Sunlights), 60 - R * np.cos (2 * i * np.pi / Sunlights)), (540 + R * np.sin ( (2 * i + 1) * np.pi / Sunlights), 60 - R * np.cos ((2 * i + 1) * np.pi / Sunlights)), ((540 + (R + r) * np.sin ((2 * i + 0.5) * np.pi / Sunlights), 60 - (R + r) * np.cos ((2 * i + 0.5) * np.pi / Sunlights)))])
+
+
+umbrella(110, 220, 8, 140, 54, 25, 15)
+umbrella(240, 245, 4, 96, 28, 28, 7)
 
 pygame.display.update()
 clock = pygame.time.Clock()
